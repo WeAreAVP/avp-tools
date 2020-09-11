c226683b0023c8fbe2377ec1018adabc5dfb0952
David Agudelo-Frankel <davidagud@gmail.com>
2020-09-11 11:04:36 -0500

    added folder/file choosing

--- srt-to-vtt-script/srt-to-vtt-script.py
+++ srt-to-vtt-script/srt-to-vtt-script.py
@@ -1,14 +1,33 @@
+'''
+Purpose: 
+
+Usage: 
+'''
+
 import os
 import re
 from datetime import datetime
 from datetime import timedelta
 
-folder = input("What is the path of the folder where the SRTs are located? ")
+use = input('Would you like to convert a single SRT or all the SRTs in a folder? Type "file" or "folder": ').strip().lower()
+
+while use != 'file' and use != 'folder':
+    print('Looks like you didn\'t type "file" or "folder". Please try running the script again')
+    use = input('Would you like to convert a single SRT or all the SRTs in a folder? Type "file" or "folder": ').strip().lower()
+
+if use == 'file':
+    input_path = input('What is the full path of the SRT file?: ').strip().replace('\\','')
+elif use == 'folder':
+    input_path = input('What is the full path of the folder containing the SRTs?: ').strip().replace('\\','')
 
 if os.name == 'posix':
-    folder = folder.replace('\\','')
+    input_path = input_path.replace('\\','')
 
-path = folder.strip().strip('"')
+path = input_path.strip().strip('"')
+
+if use == 'file':
+    file = '/'.join(path.split('/')[-1:])
+    path = '/'.join(path.split('/')[:-1])
 
 output_folder = path + '/vtts'
 
@@ -21,20 +40,8 @@ except:
     print()
     exit()
 
-for file in os.listdir(path):
-    if file[-4:] != '.srt':
-        continue
-
-    if file == '.DS_Store':
-        continue
-
-    if os.path.isdir(path + '/' + file):
-        continue
-
+def process_file(file):
     f = open(output_folder + '/' + file[:-4] + '.vtt', 'w')
-
-    offset_time = ''
-    offset_delta = ''
 
     first_line = True
 
@@ -57,6 +64,26 @@ for file in os.listdir(path):
             f.write(line)
 
     f.close()
+
+
+if use == 'file':
+    if file[-4:] != '.srt':
+        print('Sorry, file is not a .srt')
+        exit()
+    
+    process_file(file)
+elif use == 'folder':
+    for file in os.listdir(path):
+        if file[-4:] != '.srt':
+            continue
+
+        if file == '.DS_Store':
+            continue
+
+        if os.path.isdir(path + '/' + file):
+            continue
+
+        process_file(file)
 
 print()
 print('Finished converting SRTs to VTTs.')
